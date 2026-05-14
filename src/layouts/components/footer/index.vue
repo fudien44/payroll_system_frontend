67320776396ede78a708325093f2bd42ada8147d
rhynegerado <vince.rhyner1013@gmail.com>
2026-05-14 08:05:45 +0800

    created dtr module

--- src/layouts/components/footer/index.vue
+++ src/layouts/components/footer/index.vue
@@ -2,26 +2,10 @@
   <VFooter class="v-layout-footer">
     <div class="v-footer-content d-flex align-center flex-wrap">
       <span>
-<<<<<<< HEAD
-        Developed 
-        <VIcon
-          icon="mdi-language-typescript"
-          color="success"
-        />
-        by:<strong> ICT Unit</strong> <span style="color: #285c4d">Department of Health - CHD XII</span>
-      </span>
-
-      <VSpacer />
-
-      <div class="d-none d-md-block">
-        <span style="color: #285c4d">Version 1.0</span>
-      </div>
-=======
         © {{ new Date().getFullYear() }} <strong>Department of Health</strong> — Region XII (SOCCSKSARGEN). All rights reserved.
       </span>
 
       <VSpacer />
->>>>>>> 8a36dc1d4f6c62c677feb54bb21ec16dd6f3b818
     </div>
   </VFooter>
 </template>
